b25f68b132f326622219fccd90b8c8fdc2e68bb7
chunyu li <78344051+fridayL@users.noreply.github.com>
2025-09-23 16:11:13 +0800

    feat: remove  (#329)
    * fix time
    
    * add timed step for llm
    
    * fix:ci code
    
    * fix：test
    
    * remove
    
    * update
    
    * feat: remove request body
    
    * feat: remove trace id
    
    * feat: remove trace id

--- src/memos/api/product_api.py
+++ src/memos/api/product_api.py
@@ -3,7 +3,6 @@ import logging
 from fastapi import FastAPI
 
 from memos.api.exceptions import APIExceptionHandler
-from memos.api.middleware.request_context import RequestContextMiddleware
 from memos.api.routers.product_router import router as product_router
 
 
@@ -17,8 +16,6 @@ app = FastAPI(
     version="1.0.1",
 )
 
-# Add request context middleware (must be added first)
-app.add_middleware(RequestContextMiddleware)
 
 # Include routers
 app.include_router(product_router)
