7aafbd0a772c0321ae8b465c619d349dd0842287
Kai <kakack@126.com>
2025-09-26 12:06:10 +0800

    ruff format

--- src/memos/memories/textual/tree_text_memory/organize/handler.py
+++ src/memos/memories/textual/tree_text_memory/organize/handler.py
@@ -1,5 +1,6 @@
 import json
 import re
+
 from datetime import datetime
 
 from dateutil import parser
@@ -13,6 +14,7 @@ from memos.templates.tree_reorganize_prompts import (
     MEMORY_RELATION_DETECTOR_PROMPT,
     MEMORY_RELATION_RESOLVER_PROMPT,
 )
+
 
 logger = get_logger(__name__)
 
